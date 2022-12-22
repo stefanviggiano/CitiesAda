b800a2f996fa95f43a928e256f562d85cb29be92
Stefan Viggiano <stefanviggiano@gmail.com>
2022-12-22 14:06:47 -0300

    Make exercise 3

--- Cities.cs
+++ Cities.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 
 namespace Cities
@@ -18,16 +23,23 @@ namespace Cities
             string routeFileName = System.IO.Path.Combine(
                     desktopPath, "caminho.txt");
 
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = false,
+            };
             using var distancesReader = new StreamReader(distancesFileName);
-            string[] lines = distancesReader.ReadToEnd().Split();
+            using var distancesParser = new CsvParser(distancesReader, config);
 
-            int numCities = 5;
+            if (!distancesParser.Read())
+                return;
+
+            int numCities = distancesParser.Record.Length;
             var distances = new int[numCities, numCities];
+
 
             for (int i = 0; i < numCities; i++)
             {
-                string line = lines[i];
-                string[] dists = lines[i].Split(",");
+                string[] dists = distancesParser.Record;
                 for (int j = 0; j < numCities; j++)
                 {
                     int.TryParse(dists[j], out int dist);
@@ -35,18 +47,21 @@ namespace Cities
                     distances[j, i] = dist;
                 }
 
+            distancesParser.Read();
             }
 
             using var routeReader = new StreamReader(routeFileName);
-            var route = routeReader.ReadToEnd().Split(",").Select(
-                    s => int.Parse(s) - 1);
+            using var routeParser = new CsvParser(routeReader, config);
+            if (!routeParser.Read())
+                return;
+
+            var route = routeParser.Record.Select(s => int.Parse(s) - 1);
 
             var distance = 0;
             foreach (Tuple<int, int> pair in Utils.PairWise(route))
                 distance += distances[pair.Item1, pair.Item2];
             Console.WriteLine(distance);
+
         }
-
-
     }
 }
